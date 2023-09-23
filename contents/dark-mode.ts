a38b57aab6d99992357ea442f46cf09730895f53
FENG Zhichao <flankerfc@gmail.com>
2023-09-23 23:35:53 +0800

    Lint added.

--- contents/dark-mode.ts
+++ contents/dark-mode.ts
@@ -1,17 +1,15 @@
-import type { PlasmoCSConfig } from "plasmo"
-import { Storage } from "@plasmohq/storage"
+import { Storage } from '@plasmohq/storage'
+import type { PlasmoCSConfig } from 'plasmo'
 
 export const config: PlasmoCSConfig = {
-  matches: [
-    "https://*.bilibili.com/*"
-  ],
+  matches: ['https://*.bilibili.com/*'],
   run_at: 'document_start'
 }
 
-import cssText from "./dark-mode.css"
+import cssText from './dark-mode.css'
 
 export const getStyle = () => {
-  const style = document.createElement("style")
+  const style = document.createElement('style')
   style.textContent = cssText
   return style
 }
@@ -19,25 +17,23 @@ export const getStyle = () => {
 export const switchToggle = (darkBiliToggle: boolean) => {
   const htmlElement = document.getElementsByTagName('html')[0]
   if (darkBiliToggle) {
-    htmlElement.classList.add("dark-bili");
+    htmlElement.classList.add('dark-bili')
   } else {
-    htmlElement.classList.remove("dark-bili");
+    htmlElement.classList.remove('dark-bili')
   }
 
   console.log('Dark Mode swithed to ' + darkBiliToggle)
 }
 
 const storage = new Storage()
-const darkBiliToggleString = storage.get<boolean>("darkBiliToggle").then((darkBiliToggle) => {
-  if (typeof darkBiliToggle === "undefined") {
+storage.get<boolean>('darkBiliToggle').then((darkBiliToggle) => {
+  if (typeof darkBiliToggle === 'undefined') {
     switchToggle(true)
   } else {
     switchToggle(darkBiliToggle)
   }
 })
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    switchToggle(request.darkBiliToggle)
-  }
-);
+chrome.runtime.onMessage.addListener(function (request) {
+  switchToggle(request.darkBiliToggle)
+})
--- popup/index.tsx
+++ popup/index.tsx
@@ -1,33 +1,34 @@
-import { useStorage } from "@plasmohq/storage/hook"
+import React from 'react'
+import { useStorage } from '@plasmohq/storage/hook'
 import { Button, Card, Space, Switch, Typography } from 'antd'
 
-const { Text, Title } = Typography;
+import './index.css'
 
-import "./index.css"
+const { Text, Title } = Typography
 
 function IndexPopup() {
-  const [darkBiliToggle, setDarkBiliToggle] = useStorage<boolean>("darkBiliToggle")
+  const [darkBiliToggle, setDarkBiliToggle] = useStorage<boolean>('darkBiliToggle')
 
-  const toggleChecked = function(darkBiliToggle: boolean) {
+  const toggleChecked = function (darkBiliToggle: boolean) {
     setDarkBiliToggle(darkBiliToggle)
 
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {darkBiliToggle: darkBiliToggle})
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { darkBiliToggle: darkBiliToggle })
     })
   }
 
-  const websiteClicked = function() {
-    chrome.tabs.create({url: "https://flanker.github.io/bilibili-dark-mode"});
+  const websiteClicked = function () {
+    chrome.tabs.create({ url: 'https://flanker.github.io/bilibili-dark-mode' })
   }
 
-  const githubClicked = function() {
-    chrome.tabs.create({url: "https://github.com/flanker/bilibili-dark-mode"});
+  const githubClicked = function () {
+    chrome.tabs.create({ url: 'https://github.com/flanker/bilibili-dark-mode' })
   }
 
   return (
     <div
       style={{
-        minWidth: "260px",
+        minWidth: '260px',
         padding: 16
       }}>
       <Title style={{ fontSize: 24 }}>DarkBili</Title>
@@ -36,10 +37,7 @@ function IndexPopup() {
         <Card title="设置" size="small" style={{ width: 220 }}>
           <label>
             <Space>
-              <Switch
-                checked={darkBiliToggle}
-                onChange={(checked) => toggleChecked(checked)}
-              />
+              <Switch checked={darkBiliToggle} onChange={(checked) => toggleChecked(checked)} />
               <span>夜间模式</span>
             </Space>
           </label>
@@ -48,12 +46,8 @@ function IndexPopup() {
       <Space direction="vertical" size="middle">
         <Card title="插件信息" size="small" style={{ width: 220 }}>
           <Space>
-            <Button onClick={websiteClicked}>
-              访问网站
-            </Button>
-            <Button onClick={githubClicked}>
-              Github
-            </Button>
+            <Button onClick={websiteClicked}>访问网站</Button>
+            <Button onClick={githubClicked}>Github</Button>
           </Space>
         </Card>
       </Space>
